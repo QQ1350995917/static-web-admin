e21d66b27e87b4d99261b90803e2307ee4d532b5
DingPengwei <www.dingpengwei@foxmail.com>
2021-07-29 23:55:39 +0800

    stash

--- src/views/account/AdminList.vue
+++ src/views/account/AdminList.vue
@@ -21,49 +21,36 @@
             <span>{{scope.$index + query.page.index * query.page.size + 1}}</span>
           </template>
         </el-table-column>
-
+        <el-table-column width="150" label="账号" sortable>
+          <template slot-scope="scope">
+            <span>{{ scope.row.accountName }}</span>
+          </template>
+        </el-table-column>
         <el-table-column width="150" label="姓名" sortable>
           <template slot-scope="scope">
             <span>{{ scope.row.name }}</span>
           </template>
         </el-table-column>
-
         <el-table-column align="center" label="工号" width="120" sortable>
           <template slot-scope="scope">
             <span>{{ scope.row.empNo }}</span>
           </template>
         </el-table-column>
-
-        <el-table-column width="180" label="身份证号" sortable>
-          <template slot-scope="scope">
-            <span>{{ scope.row.pin }}</span>
-          </template>
-        </el-table-column>
-
         <el-table-column width="100" align="center" label="性别" sortable>
           <template slot-scope="scope">
             <span>{{ scope.row.gender }}</span>
           </template>
         </el-table-column>
-
         <el-table-column width="160" align="center" label="创建时间" sortable>
           <template slot-scope="scope">
             <span>{{ scope.row.createTime | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
           </template>
         </el-table-column>
-
         <el-table-column width="160" align="center" label="更新时间" sortable>
           <template slot-scope="scope">
             <span>{{ scope.row.updateTime | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
           </template>
         </el-table-column>
-
-        <el-table-column align="center" label="简介">
-          <template slot-scope="scope">
-            <span>{{ scope.row.summary}}</span>
-          </template>
-        </el-table-column>
-
         <el-table-column fixed="right" align="center" width="320" label="操作">
           <template slot-scope="scope">
             <el-popconfirm
@@ -132,28 +119,30 @@
         title="新增"
         :visible.sync="createMemberDialogVisible"
         :before-close="onCreateMemberDialogCloseClick">
-        <el-form :model="createMemberForm" :rules="createMemberRules" ref="createOrgMemberForm" label-width="120px">
-          <el-form-item label="账户名" prop="accountName">
+        <el-form :model="createMemberForm" :rules="createMemberRules" ref="createMemberForm" label-width="120px">
+          <el-form-item label="账号" prop="accountName" placeholder="账号不可重复">
             <el-input v-model="createMemberForm.accountName"></el-input>
           </el-form-item>
-          <el-form-item label="账户密码" prop="accountPwd">
-            <el-input v-model="createMemberForm.accountPwd"></el-input>
+          <el-form-item label="姓名" prop="userName">
+            <el-input v-model="createMemberForm.userName" placeholder="姓名可重复"></el-input>
           </el-form-item>
-          <el-form-item label="用户名" prop="userName">
-            <el-input v-model="createMemberForm.userName"></el-input>
+          <el-form-item label="手机号码" prop="cellphone">
+            <el-input v-model="createMemberForm.cellphone" placeholder="手机号码不可重复"></el-input>
           </el-form-item>
-          <el-form-item label="身份证号" prop="pin">
-            <el-input v-model="createMemberForm.pin"></el-input>
+          <el-form-item label="邮箱" prop="email">
+            <el-input v-model="createMemberForm.email" placeholder="邮箱号码不可重复"></el-input>
           </el-form-item>
-          <el-form-item label="工号" prop="empNo">
-            <el-input v-model="createMemberForm.empNo"></el-input>
-          </el-form-item>
-
           <el-form-item label="性别" prop="gender">
             <el-radio-group v-model="createMemberForm.gender">
-              <el-radio label="0"></el-radio>
-              <el-radio label="1"></el-radio>
+              <el-radio label="0">女</el-radio>
+              <el-radio label="1">男</el-radio>
             </el-radio-group>
+          </el-form-item>
+          <el-form-item label="身份证号" prop="pin">
+            <el-input v-model="createMemberForm.pin" placeholder="身份证号不可重复"></el-input>
+          </el-form-item>
+          <el-form-item label="工号" prop="empNo">
+            <el-input v-model="createMemberForm.empNo" placeholder="工号不可重复"></el-input>
           </el-form-item>
           <el-form-item label="简介" prop="summary">
             <el-input type="textarea" v-model="createMemberForm.summary"></el-input>
@@ -161,8 +150,8 @@
         </el-form>
         <span slot="footer" class="dialog-footer">
           <el-button @click="onCreateMemberDialogResetClick('createMemberForm')">重置</el-button>
-          <el-button @click="onCreateMemberDialogCloseClick">取 消</el-button>
-          <el-button type="primary" @click="onCreateMemberDialogConfirmClick('createMemberForm')">确 定</el-button>
+          <el-button @click="onCreateMemberDialogCloseClick()">取消</el-button>
+          <el-button type="primary" @click="onCreateMemberDialogConfirmClick('createMemberForm')">创建</el-button>
       </span>
       </el-dialog>
     </el-main>
@@ -210,11 +199,11 @@
         total: 0,
         selectAll: false,
         createMemberDialogVisible: false,
-        onCreateMemberDialogCloseClick: false,
         createMemberForm: {
           accountName: '',
-//          accountPwd: '',
           userName: '',
+          cellphone: '',
+          email: '',
           pin: '',
           empNo: '',
           level: 1,
@@ -226,13 +215,39 @@
             {required: true, message: '请输入账户名称', trigger: 'blur'},
             {min: 4, max: 24, message: '长度在 4 到 24 个字符', trigger: 'blur'}
           ],
-//          accountPwd: [
-//            {required: true, message: '请输入账户密码', trigger: 'blur'},
-//            {min: 6, max: 18, message: '长度在 6 到 18 个字符', trigger: 'blur'}
-//          ],
           userName: [
-            {required: true, message: '请输入用户名称', trigger: 'blur'},
+            {required: true, message: '请输入姓名', trigger: 'blur'},
             {min: 2, max: 20, message: '长度在 2 到 20 个字符', trigger: 'blur'}
+          ],
+          cellphone: [
+            {required: true, message: '请输入手机号', trigger: 'blur'},
+            {
+              validator: function(rule, value, callback) {
+                if (/^((13[0-9])|(15([0-3]|[5-9]))|(166)|(17[0,1,3,5,6,7,8])|(18[0-9])|(19[8|9]))\\d{8}$/.test(value) == false) {
+                  callback(new Error("手机号格式错误"));
+                } else {
+                  callback();
+                }
+              },
+              trigger: "blur"
+            }
+          ],
+          email: [
+            {required: true, message: '请输入邮箱', trigger: 'blur'},
+            {
+              validator: function(rule, value, callback) {
+                if (
+                  /^\w{1,64}@[a-z0-9\-]{1,256}(\.[a-z]{2,6}){1,2}$/i.test(
+                    value
+                  ) == false
+                ) {
+                  callback(new Error("邮箱格式错误"));
+                } else {
+                  callback();
+                }
+              },
+              trigger: "blur"
+            }
           ],
           pin: [
             {required: false, message: '请输入用户证件号', trigger: 'blur'},
@@ -343,26 +358,26 @@
       onCreateMemberButtonClick() {
         this.createMemberDialogVisible = true;
       },
-      onCreateMemberDialogCloseClick(){
+      onCreateMemberDialogResetClick(formName) {
+        this.$refs[formName].resetFields();
+      },
+      onCreateMemberDialogCloseClick() {
         this.createMemberDialogVisible = false;
       },
       onCreateMemberDialogConfirmClick(formName) {
         this.$refs[formName].validate((valid) => {
           if (valid) {
-            this.$store.dispatch('account/create', this.adminForm)
+            this.$store.dispatch('account/create', this.createMemberForm)
               .then((response) => {
                 if (response.meta.code == 200) {
                   this.$router.push({path: this.redirect || '/account/admin/list'})
                 }
-              }).catch(() => {
+              }).catch((e) => {
             })
           } else {
             return false;
           }
         });
-      },
-      onCreateMemberDialogResetClick(formName) {
-        this.$refs[formName].resetFields();
       },
     },
   }
